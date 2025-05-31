014ee4305bdb96e25b6ac8d7929d71ee9e0e1118
Cramess <cristianrufinoccencho@gmail.com>
2025-05-30 20:45:42 -0500

    Add files via upload
    LABORATORIO N°10
    Motor de renderizado en Laravel
    
    Garcia Ccencho Cristian Rufino

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\GameController; 
+use App\Http\Controllers\ProfileController; 
+use Illuminate\Support\Facades\Route;
+
+   Route::get('/games', [GameController::class, 'index']);
+   Route::get('/games/create', [GameController::class, 'create']); 
+   Route::get('/games/list', [GameController::class, 'list']); 
+   Route::get('/games/{game}/{category?}', [GameController::class, 'help']);
+   Route::post('/games/store', [GameController::class, 'store'])->name('games.store');
